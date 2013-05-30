0ab2dc29bd6b4fe80fe3d7115a000fc3655fe9db
Viswajith <viswajithiii@gmail.com>
2013-05-30 14:24:59 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -31,13 +31,20 @@ HOSTEL_CHOICES  =(
 #User Profile Model
 class ERPUser(models.Model):
     user = models.OneToOneField(User)
-    dept = models.ForeignKey(Dept, related_name='dept')
-    subdept = models.ForeignKey(Subdept, blank=True, null=True, default=None, related_name='subdept')
+   # dept = models.ForeignKey(Dept, related_name='dept')
+   # subdept = models.ForeignKey(Subdept, blank=True, null=True, default=None, related_name='subdept')
 
+#I think instead of focussing on multiple identities as a specific case, we should take that
+#as the general case, of which single identity is a special case.
+#About why I'm changing the Foreign Keys above: Django DB will throw errors if you have
+#two different relationship fields to the same model. Moreover, it is redundant.
+#Hence I've added core_relations as a ForeignKey below ( since a core is always one core of one department )
+#I've temporarily only commented out the above lines for clarity. Delete them off after you've understood.
     #Handling the Multiple Identity Problem
     multiple_ids = models.BooleanField(default=False)
     coord_relations = models.ManyToManyField(Subdept, related_name='coord_relations')
     supercoord_relations = models.ManyToManyField(Dept, related_name='supercoord_relations')
+    core_relations = models.ForeignKey(Dept, related_name='core_set')
 
     #Other information
     nickname = models.CharField(max_length=30, blank=True)

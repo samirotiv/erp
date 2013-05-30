6713f49e4b332925aab7d937affe336a4e3c23a0
Viswajith <viswajithiii@gmail.com>
2013-05-30 13:05:37 +0530

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -29,16 +29,10 @@ class Task(models.Model):
     targetdept = models.ForeignKey(Dept, related_name='targetdept')
     isxdepartmental = models.BooleanField(default=False)
     
-# STILL UNDECIDED - DO WE NEED ALL THESE BOOLEANS? ONE INTEGER/STRING CAN DO THE TRICK.
-# MADE ALL OF THEM ANYWAY
-# TASKSTATUS KEPT AS INTEGERFIELD
-# TODO: VISWA PLEASE SET IT AS YOU LIKE TO A STRING OR WHATEVER SUITS YOU.
-    #Task Status
-    taskapproved = models.BooleanField(default=False)
-    taskcompleted = models.BooleanField(default=False)
-    taskstatus = models.IntegerField()
-    #We need to change the taskstatus variable to
-    #taskstatus = models.CharField ( max_length=1, choices=TASK_STATUSES )
+    #This is the task status variable.
+    #One question is what it defaults to. This depends on isxdepartmental.
+    #TODO: Write a method that handles this.
+    taskstatus = models.CharField ( max_length=1, choices=TASK_STATUSES )
 
     #Task Nesting
     parenttask = models.ForeignKey('self', null=True, blank=True, default=None, related_name='parent_task')

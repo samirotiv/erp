3219c03e43ca9489b73cea936b2666cc0a6732c4
Samir Otiv <samirotiv@fakeserver.com>
2013-05-30 21:07:36 +0530

    Lots of changes - Admin, Models, Views (Incomplete)
    TODO: Add null=true for models at some places.

--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+# ************** ERP - TASKS APP - VIEWS *********************
+from tasks import Task
+
+
+def core_check (user):
+    loginuser = user.get_profile()
+    return loginuser.status == 2
+
+
+# _____________--- TASK ADD VIEW ---______________#
+@login_required
+@user_passes_test (core_check)
+def AddTask(request):
+    if request.method == 'POST':
+        form= TaskForm(request.POST)
+        newTask = form.save()
+        return HttpResponseRedirect('/home')
+    else:
+        form=TaskForm()
+        context = {'form': form}
+        return render_to_response('tasks/task.htm', context, context_instance=RequestContext(request))
--- users/models.py
+++ users/models.py
@@ -33,32 +33,7 @@ class ERPUser(models.Model):
     user = models.OneToOneField(User)
     dept = models.ForeignKey(Dept, related_name='dept_user_set')
     subdept = models.ForeignKey(Subdept, blank=True, null=True, default=None, related_name='subdept_user_set')
-
-#I think instead of focussing on multiple identities as a specific case, we should take that
-#as the general case, of which single identity is a special case.
-#About why I'm changing the Foreign Keys above: Django DB will throw errors if you have
-#two different relationship fields to the same model. Moreover, it is redundant.
-#Hence I've added core_relations as a ForeignKey below ( since a core is always one core of one department )
-#I've temporarily only commented out the above lines for clarity. Delete them off after you've understood.
-    
-    
-#<SAMIR>
-# I've given this a lot of thought - I didn't say this, but I had always intended to take multiple identities as the general case.
-# It makes sense to let the above two fields be since they tell you WHICH identity the user is assuming currently.
-# By taking out the above two lines, you won't know what the user is logged into.
-# This is going to be a problem unless you want to create a unified login - where a user sees information from all his accounts.
-# I have considered this - I conclude it's going to be more painful since a user may be a supercoord of two subdepts - in which case
-# for every little thing we'll have to worry about what Identity he wants to use. eg. Create a task as a Supercoord of which department?
-#
-# I'm changing it back to the original model for the time being. We'll discuss further in the evening if necessary.
-#
-# It should be a lot easier just to allow the user to assume multiple identies instead of creating a unified dashboard
-# So we need those fields. 
-# It's fine if the core_relations remain, just in case.
-#
-# And about the Django Error - that is taken care of by related_name
-# </SAMIR>
-    
+    status = models.IntegerField (default=0) # 0 = Coord, 1 = Supercoord, 2 = Cor
     
     #Handling the Multiple Identity Problem
     multiple_ids = models.BooleanField(default=False)

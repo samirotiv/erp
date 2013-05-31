07b77187271625fe408ab776dfe9c955f09ec33f
Viswa <viswa@viswa-Satellite-L850.(none)>
2013-05-31 20:35:00 +0530

    Corrected a bug

--- home/views.py
+++ home/views.py
@@ -9,8 +9,8 @@ def homepage ( request ):
 def choose_identity ( request ):
     
     if request.method == 'POST':
-	print 'Post is happening :D'
-	if identity_form.is_valid:
+	identity_form = ChooseIdentityForm ( request.POST )
+	if identity_form.is_valid():
 	    return HttpResponseRedirect ( '/home')
     identity_form = ChooseIdentityForm ( { 'coord_relations':request.user.erpuser.coord_relations, 'supercoord_relations':request.user.erpuser.supercoord_relations})
     return render_to_response ( 'home/choose_identity.html', locals(), context_instance = RequestContext(request) )

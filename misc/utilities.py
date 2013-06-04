6872ef18ce8e078e94bc9161d66b9742347f0e2b
Samir Otiv <samirotiv@fakeserver.com>
2013-06-04 20:34:25 +0530

    Fixed errors in Check Utilities and relocated Static Files

--- misc/utilities.py
+++ misc/utilities.py
@@ -3,9 +3,15 @@
 # _____________--- CORE CHECK FUNCTION ---______________#
 def core_check (user):
     loginuser = user.get_profile()
-    return loginuser.status == 2
+    if loginuser.status == '2':
+        return True
+    else:
+        return False
 
 def core_or_supercoord_check (user):
     loginuser = user.get_profile()
-    return ((loginuser.status == 2) or (loginuser.status == 1))
+    if ((loginuser.status == '2') or (loginuser.status == '1')):
+        return True
+    else:
+        return False
 

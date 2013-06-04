98dd2800fcf7c623cbfc9fe054fb0c2cf3af4b5a
Samir Otiv <samirotiv@fakeserver.com>
2013-06-04 19:41:30 +0530

    Made significant improvements to Task pages - delete_task view created, made the views more user friendly & added supercoord functionality

--- tasks/views.py
+++ tasks/views.py
@@ -8,15 +8,20 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.template import RequestContext
 
-from misc.utilities import core_check
+from misc.utilities import core_check, core_or_supercoord_check
+
+
+
 
 
 """
 PROPOSED/TODO:
 1. TO ALSO AUTOMATE THE SETTING OF CHILD TASK.
 2. Edit task - create redictor for cross/intra departmental?
-3. Add supercoords into the game
-4. Disallow cores from being given tasks?
+3. Add supercoords into the game - DONE
+4. Disallow cores from being given tasks - NO, CORES CAN BE GIVEN TASKS.
+5. Also look below for more TODOs
+6. Add strings like Page Heading to the dictionary that is rendered - since one template is being used for all.
 """
 
 
@@ -34,21 +39,23 @@ Fields taken care of by the view:
     'targetsubdepts', 'origindept', 'targetdept', 'isxdepartmental', 'taskstatus'
 """
 @login_required
-@user_passes_test (core_check)
+@user_passes_test (core_or_supercoord_check)
 def add_intra_task(request):
 
     userprofile = request.user.get_profile()
     department = userprofile.dept
+    title = "Add Intradepartmental Task"
     
     if request.method == 'POST':
         form = IntraTaskForm(department, request.POST)
         if form.is_valid():
             newTask = form.save(commit=False)
 
-            #Set the origin & target departments.        
+            #Set the origin & target departments & approve the task.        
             newTask.origindept = userprofile.dept
             newTask.targetdept = userprofile.dept
             newTask.taskcreator = userprofile
+            newTask.taskstatus = 'O'
             
             #For many to many relationships to be created, the object MUST first exist in the database.
             newTask.save()
@@ -67,24 +74,15 @@ def add_intra_task(request):
                 newTask.taskforce.add(user)
             for user in cores: 
                 newTask.taskforce.add(user)
-      
-
-            
-            # Add the concerned subdepartments to the "targetsubdepts" field.
-            newTask.populateTargetSubdepts()
-
-            #Set these variables - Approved & Ongoing Intra-departmental task.
-            newTask.isxdepartmental = False
-            newTask.taskstatus = 'O'
         
             newTask.save()        
             return HttpResponse ("Task Saved")
         else:
-            return render_to_response ('tasks/task.html', {'form': form }, context_instance=RequestContext(request))
+            return render_to_response ('tasks/task.html', {'form': form, 'title':title }, context_instance=RequestContext(request))
     
     else:
         form = IntraTaskForm(department)
-        context = {'form': form}
+        context = {'form': form, 'title':title }
         return render_to_response('tasks/task.html', context, context_instance=RequestContext(request))
 
 
@@ -126,14 +124,15 @@ Fields that are unset:
      'taskforce'
 """
 @login_required
-@user_passes_test (core_check)
+@user_passes_test (core_or_supercoord_check)
 def edit_task(request, primkey):
     task = Task.objects.get(pk=primkey)
     userprofile = request.user.get_profile()
     department = userprofile.dept
 
 #___________----INTRADEPARTMENTAL TASK EDIT----__________________
-    if (task.isxdepartmental == False) and (task.origindept == department):
+    if ((task.isxdepartmental == False) and (task.origindept == department)):
+        title = "Edit Intradepartmental Task"
         if request.method == 'POST':
             form = IntraTaskForm(department, request.POST, instance=task)
             if form.is_valid():
@@ -153,26 +152,30 @@ def edit_task(request, primkey):
                     task.taskforce.add(user)
                 for user in cores: 
                     task.taskforce.add(user)
-                
-                # Add the concerned subdepartments to the "targetsubdepts" field.
-                task.populateTargetSubdepts()
 
                 task.save()        
                 return HttpResponse ("Task Saved")
             else:
                 #Render the form along with all its errors.
-                return render_to_response ('tasks/task.html', {'form': form }, context_instance=RequestContext(request))
+                return render_to_response ('tasks/task.html', {'form': form, 'title':title  }, context_instance=RequestContext(request))
         
         else:
             form = IntraTaskForm(department, instance=task)
-            context = {'form': form}
+            context = {'form': form, 'title':title }
             return render_to_response('tasks/task.html', context, context_instance=RequestContext(request))
             
-#___________----CROSSDEPARTMENTAL TASK EDIT----__________________            
-    if task.isxdepartmental == True:
+#___________----CROSSDEPARTMENTAL TASK EDIT----__________________
+
+#TODO: 
+#For the target core's beenfit, display the ASSIGNED SUBDEPARTMENT in the Approval Page.
+#As of now, he has the freedom to assign the task to users from other subdepartments too.
+
+    elif (task.isxdepartmental == True) and (core_check(request.user)):
         if request.method == 'POST':
             #For the originating department's core
             if (task.taskstatus == 'U') and (task.origindept == department):
+                title = "Edit Crossdepartmental Task"
+                info = "Allowed only until approved by the target department's core."
                 form = CrossTaskForm(department, request.POST, instance=task)
                 if form.is_valid():
                     form.save()
@@ -185,10 +188,15 @@ def edit_task(request, primkey):
                     return HttpResponse ("Task Saved")
                 else:
                     #Render the form again with all its errors.
-                    return render_to_response ('tasks/task.html', {'form': form }, context_instance=RequestContext(request))
+                    return render_to_response ('tasks/task.html', {'form': form, 'title':title, 'info': info  }, context_instance=RequestContext(request))
             
             #For the target department's core:
             if task.targetdept == department:
+                #Getting the targetsubdept. There is only one. The loop will run only once.
+                for subdept in task.targetsubdepts:
+                    targetsubdept = subdept
+                title = "Edit/Approve Crossdepartmental Task"
+                info = "Submitting the task here automatically approves & assigns it to the selected workforce.\n<b>Target Subdepartment Requested for the Task: " + targetsubdept + "</b>"
                 form = IntraTaskForm(department, request.POST, instance=task)
                 if form.is_valid():
                     form.save()
@@ -210,12 +218,12 @@ def edit_task(request, primkey):
                     
                     #Approve the task.
                     task.taskstatus = 'O'
-                    
-                    # Add the concerned subdepartments to the "targetsubdepts" field.
-                    task.populateTargetSubdepts()
 
                     task.save()        
                     return HttpResponse ("Task Saved/Approved.")
+                else:
+                    #Render the form again with all its errors.
+                    return render_to_response ('tasks/task.html', {'form': form, 'title':title , 'info': info  }, context_instance=RequestContext(request))
             
             else:
                 return HttpResponse ("Pack.")
@@ -223,19 +231,28 @@ def edit_task(request, primkey):
         else:
             #For the originating department's core
             if (task.taskstatus == 'U') and (task.origindept == department):
+                title = "Edit Crossdepartmental Task"
+                info = "Allowed only until approved by the target department's core."
                 form = CrossTaskForm (department, instance=task)
-                context = {'form': form}
+                context = {'form': form, 'title':title , 'info': info }
                 return render_to_response('tasks/task.html', context, context_instance=RequestContext(request))
             
             #For the target department's core:
             if task.targetdept == department:
+                #Getting the targetsubdept. There is only one. The loop will run only once.
+                for subdept in task.targetsubdepts:
+                    targetsubdept = subdept
+                title = "Edit/Approve Crossdepartmental Task"
+                info = "Submitting the task here automatically approves & assigns it to the selected workforce.\n<b>Target Subdepartment Requested for the Task: " + targetsubdept + "</b>"
                 form = IntraTaskForm(department, instance=task)
-                context = {'form': form}
+                context = {'form': form, 'title':title , 'info': info }
                 return render_to_response('tasks/task.html', context, context_instance=RequestContext(request))
                 
             else:
                 return HttpResponse ("Pack.")
 
+    else: 
+        return HttpResponse ("Pack.")
     
 
         
@@ -259,7 +276,9 @@ Fields that are unset:
 @login_required
 @user_passes_test (core_check)
 def add_cross_task(request):
-
+    title = "Add Cross-departmental Task."
+    info = "Subject to approval of the target department's core."
+    
     userprofile = request.user.get_profile()
     department = userprofile.dept
     
@@ -290,10 +309,10 @@ def add_cross_task(request):
             return HttpResponse ("Task Saved")
         else:
             #Render the form again with all its errors.
-            return render_to_response ('tasks/task.html', {'form': form }, context_instance=RequestContext(request))
+            return render_to_response ('tasks/task.html', {'form': form, 'title':title, 'info':info  }, context_instance=RequestContext(request))
     else:
         form = CrossTaskForm (department)
-        context = {'form': form}
+        context = {'form': form, 'title':title, 'info':info }
         return render_to_response('tasks/task.html', context, context_instance=RequestContext(request))
         
         
@@ -301,14 +320,30 @@ def add_cross_task(request):
 
 
 # _____________--- TASK DISPLAY VIEW ---______________#
-def display_task(request,primkey):
+@login_required
+def display_task(request, primkey):
 
 #TODO: Redirect people who aren't allowd to view this task. Add edit and delete buttons for cores and supercoords
+#Display ALL details in the template - template needs work.
 
     try:
-	task = Task.objects.get(pk = primkey)
+        task = Task.objects.get(pk = primkey)
     except:
-	return HttpResponse ("That task does not exist.")
+        return HttpResponse ("That task does not exist.")
     return render_to_response('tasks/display.html', locals() )
 
+
+# _____________--- TASK DELETE VIEW ---______________#
+@login_required
+@user_passes_test (core_check)
+def delete_task(request, primkey):
+    try:
+        task = Task.objects.get(pk = primkey)
+    except:
+        return HttpResponse ("That task does not exist.")
+    
+    task.delete()
+    return HttpResponse ("Task Deleted")
+    
+#TODO: WRITE CODE.
 

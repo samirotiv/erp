fd19dac68770ddee8dc53ade3fcb28a778122a5d
Samir Otiv <samirotiv@fakeserver.com>
2013-06-04 19:40:08 +0530

    Fixed URLs

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('tasks.views',
-    url(r'(?P<primkey>\d+)/$', 'display_task'),
     url(r'^add/$', 'add_intra_task'),
     url(r'^addx/$', 'add_cross_task'),  
     url(r'^edit/(?P<primkey>\d+)/$', 'edit_task'),                    
-                                          
+    url(r'^display/(?P<primkey>\d+)/$', 'display_task'), 
+    url(r'^del/(?P<primkey>\d+)/$', 'delete_task'), 
+                                                                                   
 #  url(r'^addcopy/$', 'libauth.views.AddCopy'),
 # url(r'^updatecopy/(?P<primkey>\d+)/$', 'libauth.views.UpdateCopy'),
 )

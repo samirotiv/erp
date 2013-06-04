2928f4974d4fa00dae10308abf83ccc7af54fe83
Samir Otiv <samirotiv@fakeserver.com>
2013-06-04 19:39:10 +0530

    Added Check Utility

--- misc/utilities.py
+++ misc/utilities.py
@@ -5,4 +5,7 @@ def core_check (user):
     loginuser = user.get_profile()
     return loginuser.status == 2
 
+def core_or_supercoord_check (user):
+    loginuser = user.get_profile()
+    return ((loginuser.status == 2) or (loginuser.status == 1))
 
